e48f93cbc0f6145a42a30039f667f3e5e6874c4e
hesam.mahmoodi <hesam.mahmoodi@gmail.com>
2022-12-31 10:01:22 +0330

    button click to change background

---
+++ src/components/body.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import styles from "./body.module.scss";
+
+const Body = () => {
+  const [checkBackgound, setCheckBackground] = useState(false);
+  const ChangeColorHandler = () => {
+    checkBackgound === false
+      ? (document.getElementById("body").style.background = "gold")
+      : (document.getElementById("body").style.background = "white");
+    setCheckBackground(!checkBackgound);
+  };
+  return (
+    <div id="body" className={styles.body}>
+      <h1>Click the Button Bellow</h1>
+      <button
+        onClick={() => {
+          ChangeColorHandler();
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default Body;
